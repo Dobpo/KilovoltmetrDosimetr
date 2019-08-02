0dd6eb33e0b77f1b91ab89b3212a43ff46c67fa1
Dobpo <duden89@gmail.com>
2019-08-02 16:22:56 +0300

    Initial commit

---
+++ app/src/main/java/com/idobro/kilovoltmetr_dosimetr/MainActivity.java
@@ -0,0 +1,13 @@
+package com.idobro.kilovoltmetr_dosimetr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
